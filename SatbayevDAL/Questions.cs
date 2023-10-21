5b737900cdb4d90752d8b939a64536bd406ed7fd
samet <samet@DESKTOP-OEAKA56>
2023-10-21 16:00:03 +0600

    Add project files.

---
+++ SatbayevDAL/Questions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatbayevDAL
+{
+    public class QuestionTemplate
+    {
+        public int id {  get; set; }
+        public string author { get; set; }
+        public DateTime createDate { get; set; }
+        public string questions { get; set; }
+        public double mark { get; set; }
+        public string level { get; set; }
+        public string changeAuthor { get; set; }
+        public DateTime changeDate { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+        
+
+    }
+}
---
+++ SatbayevDAL/Repository.cs
@@ -0,0 +1,65 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatbayevDAL
+{
+    public class Repository
+    {
+        
+        public bool CreateQuiestions(QuestionTemplate question)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(" "))
+                {
+                    var col = db.GetCollection<QuestionTemplate>("Questions");
+                    col.Insert(question);
+                    
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        public bool UpdateQuestions(QuestionTemplate question)
+        {
+            try
+            {
+                using(var db = new LiteDatabase(" "))
+                {
+                    var col = db.GetCollection<QuestionTemplate>(" ");
+                    col.Update(question);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool DeleteQuestions(QuestionTemplate question)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(" "))
+                {
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
